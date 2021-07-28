be8194c0a24dff8dcd44998699fff55091ef7f04
shubhamJawale <shubhamjawale11@gmail.com>
2021-07-29 00:03:38 +0530

    button addition

--- src/App.js
+++ src/App.js
@@ -2,24 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let list = [];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      < h1 > trail app</h1 >
+      <button> press me</button>
     </div>
-  );
+  )
 }
 
 export default App;
