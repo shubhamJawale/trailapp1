96d99d825e17637aad2084448610c631fb21a5f6
shubhamJawale <shubhamjawale11@gmail.com>
2021-07-29 02:17:49 +0530

    function

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 
 function App() {
   let list = [];
-
+  function func() { return alert('hello'); };
   return (
 
     <div>
       < h1 > trail app</h1 >
-      <button> press me</button>
+      <button onClick={func}> press me</button>
     </div>
   )
 }
