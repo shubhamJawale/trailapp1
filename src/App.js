b2f8e3678b38fd0125bff1a3d6df72e19c72983b
shubhamJawale <shubhamjawale11@gmail.com>
2021-07-29 16:38:01 +0530

    statefull example

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import { upstate, useState } from "react";
 
 // this program demonstrates a staless vrables even though syntactally iit is correct but the c0ounter is limited with scope and its value can not be updated
 function App() {
-  let count = 0;
+  let [count, setCount] = useState(0);
 
   let incr = () => {
     console.log(count);
     count += 1;
+    setCount(count);
   }
 
+
+  let rset = () => {
+    count = 0;
+    setCount(count);
+  }
 
   return (
 
@@ -18,6 +24,7 @@ function App() {
     <div>
       < h1 > trail app</h1 >
       <button onClick={incr}> press me</button>
+      <button onClick={rset}> reset</button>
       <h1>{count}</h1>
 
     </div>
