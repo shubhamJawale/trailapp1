20984fa556a9d1644523b7cd27e20185ea07c010
shubhamJawale <shubhamjawale11@gmail.com>
2021-07-29 15:53:50 +0530

    map added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let list = [];
+  let list = [1, 2, 3, 4];
   function func() { return alert('hello'); };
   return (
 
     <div>
       < h1 > trail app</h1 >
       <button onClick={func}> press me</button>
+      {list.map((item) => {
+        return <h1>{item}*{item}</h1>;
+      })}
     </div>
   )
 }
