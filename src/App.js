7baa70b77fa9823fd4e0c42fa39a59e5e84efead
shubhamJawale <shubhamjawale11@gmail.com>
2021-07-30 02:56:51 +0530

    complete app

--- src/App.js
+++ src/App.js
@@ -1,41 +1,59 @@
 import "./App.css";
 import { upstate, useState } from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+//import { BrowserRouter, Route, Link } from "react-router-dom";
 //npm install react-router-dom
-import { Page1, Page2, Page3, Page4 ,Page5 } from "./pages.js";
+//import { Page1, Page2, Page3, Page4 ,Page5 } from "./pages.js";
 
-function App() {
+
+
+export default function App() {
+  let [todolist, setlist]  = useState([]);
+let [textinp, setinp] =useState("");
+
+let clk= () =>{
+  let listchng = [textinp ,...todolist];
+  setlist(listchng);
+  let str = "";
+  setinp(str);
+
+}
+
+  let chnge =(e) =>{
+    let textinp1 = e.target.value;
+   
+    console.log(textinp1); 
+    setinp(textinp1);
+    
+  }
+
+  let rstclr =() =>
+  {
+    setlist([]);
+  }
+
+ 
+
   return (
-    <div>
-      
-      
-      <BrowserRouter>
-      <NavMenu />
-       
-        <Route exact={true} path="/page1" component={Page1} />
-        <Route exact={true} path="/page2" component={Page2} />
-        <Route exact={true} path="/page3" component={Page3} />
-        <Route exact={true} path="/page4" component={Page4} />
-        <Route exact={true}  path="/page5" component={Page5}  />
-        <Route exact={true} path="/" component={Page1} />
-      </BrowserRouter>
+    <div className="mx-5">
+      <div className="border border-danger p-5 mt-5">
+      <Heading/>
+      <input type="text" name="" id="" className="form-control" value={textinp} onChange={chnge} placeholder="Enter Task !  !   !   !  !"/>  
+      <button className="btn btn-outline-danger mt-3 px-5" onClick={clk}>Add Note</button> 
+      <button className="btn btn-outline-warning mx-3 mt-3 px-5" onClick={rstclr}>Clear List</button> 
+        </div>  
+        <div className="border border-success p-5 mt-5">
+          <h1 className="alert alert-warning px-5"> TASKS </h1>
+         {todolist.map((item)=>{return <h1 className="alert alert-success mt-2">{item}</h1>})}
+        </div>
       
     </div>
   );
 }
 
-//component
-// name of component should be in pascal case always
-function NavMenu() {
-  return (
-    <div className="navbar navbar-expand-lg navbar-dark bg-dark text-light">
-      <Link to="/page1" className="text-light p-5"> page1 </Link>
-      <Link to="/page2" className="text-light p-5"> page2 </Link>
-      <Link to="/page3" className="text-light p-5"> page3 </Link>
-      <Link to="/page4" className="text-light p-5"> page4 </Link>
-      <Link to="/page5" className="text-light p-5" > page5 </Link>
-    </div>
-  );
-}
 
-export default App;
+// component
+function Heading() 
+{
+  return (<div className="bg-danger px-5 text-light " > <h1> TO DO LIST</h1> </div>)
+}
+//export default App;
