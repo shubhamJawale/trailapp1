9e1b972297b924a13ede647808e542c202fce100
shubhamJawale <shubhamjawale11@gmail.com>
2021-07-29 20:20:59 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 import { upstate, useState } from "react";
 
 // this program demonstrates a staless vrables even though syntactally iit is correct but the c0ounter is limited with scope and its value can not be updated
+
+
+
+
+
+
+
+
 function App() {
   let [count, setCount] = useState(0);
 
@@ -23,9 +31,9 @@ function App() {
 
     <div>
       < h1 > trail app</h1 >
-      <button onClick={incr}> press me</button>
+      <button onClick={incr}>  press me</button>
       <button onClick={rset}> reset</button>
-      <h1>{count}</h1>
+      <h1> (¬‿¬) {count}</h1>
 
     </div>
   )
