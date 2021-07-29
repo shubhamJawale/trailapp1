565be08b7d541b314ece4b18ee11cd327f49dd69
shubhamJawale <shubhamjawale11@gmail.com>
2021-07-29 16:34:00 +0530

    counter program stateless

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { upstate, useState } from "react";
 
+// this program demonstrates a staless vrables even though syntactally iit is correct but the c0ounter is limited with scope and its value can not be updated
 function App() {
-  let list = [1, 2, 3, 4];
-  function func() { return alert('hello'); };
+  let count = 0;
+
+  let incr = () => {
+    console.log(count);
+    count += 1;
+  }
+
+
   return (
+
 
     <div>
       < h1 > trail app</h1 >
-      <button onClick={func}> press me</button>
-      {list.map((item) => {
-        return <h1>{item}*{item}</h1>;
-      })}
+      <button onClick={incr}> press me</button>
+      <h1>{count}</h1>
+
     </div>
   )
 }
