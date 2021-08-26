fd36239e9dbb3c9d399b67ee72a298e309024dd9
shubhamJawale <shubhamjawale11@gmail.com>
2021-08-26 22:29:40 +0530

    validation failed

--- src/App.js
+++ src/App.js
@@ -3,11 +3,49 @@ import "./App.css";
 import { useState } from "react";
 import Form from "./form.js"
 import RegisterForm from "./form1";
+import axios from "axios";
+import { useEffect } from "react";
+import { start } from "@popperjs/core";
 
 export default function App() {
+  const [list, setlist] = useState([]);
+
+
+
+
+  let test = () => {
+    axios.get("http://localhost:9096/gp").then(
+      (response) => {
+        console.log(response);
+        setlist(...list, response.data);
+        //console.log(list);
+      },
+      (error) => {
+        console.log("be5ta error alla");
+      }
+
+
+    )
+
+  }
+  useEffect(() => {
+    test();
+  });
+
   return (
     <div>
-      <RegisterForm />
+      {
+
+
+        list.map((item) => {
+
+          return `<h1 key={prodId}>${item.prodId}</td>`;
+
+        }
+
+        )
+
+      }
     </div>
   );
 }
