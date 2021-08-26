ca07ade1827b087157ddcd455dcfe634669f11e5
shubhamJawale <shubhamjawale11@gmail.com>
2021-08-26 12:58:43 +0530

    form validation using class

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@
 import "./App.css";
 import { useState } from "react";
 import Form from "./form.js"
+import RegisterForm from "./form1";
 
 export default function App() {
   return (
     <div>
-      <Form />
+      <RegisterForm />
     </div>
   );
 }
