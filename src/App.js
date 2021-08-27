2685ab9179f1ea5ee954d1e7e5d5f6a71f751788
shubhamJawale <shubhamjawale11@gmail.com>
2021-08-27 12:45:49 +0530

    fully working ahe

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import axios from "axios";
 import { useEffect } from "react";
 // import { start } from "@popperjs/core";
 import { Cardb } from "./cadrfile";
+import { ToastContainer, toast } from 'react-toastify';
 
 export default function App() {
-  const [list, setlist] = useState([{ prodId: 0, productName: 'shubham', productCategory: 'shubham', price: 23.45 }]);
+  const [list, setlist] = useState([{}]);
 
-  setlist(...list, { prodId: 1, productName: 'shubham', productCategory: 'shubham', price: 23.45 });
 
 
   let test = () => {
@@ -20,12 +20,17 @@ export default function App() {
         //console.log(response);
         let data = response.data;
         console.log(response.data[0]);
+        // let list2 = [{ prodId: 1, productName: 'shubham', productCategory: 'shubham', price: 23.45 }];
+        setlist(response.data);
+        toast.success('Data Loaded');
+
         //setlist(...list, { prodId: 0, productName: 'shubham', productCategory: 'shubham', price: 23.45 });
         //console.log(list);
       },
       (error) => {
         console.log(error);
         console.log("be5ta error alla");
+        toast.error('Sorry Something Went Wrong');
       }
 
 
@@ -34,27 +39,37 @@ export default function App() {
   }
   useEffect(() => {
     test();
-  });
+  }, []);
 
   let [list1, setlist1] = useState([])
 
   let i1 = 0;
   list1 = ['hello', 'shubham', 'jawale'];
   return (
-    <div>
+    <div className="container">
+      <ToastContainer />
       {
-        list.map((item) => {
-          return (<div key={item.prodId}>
-            <p> {item.prodId}
-              {item.productName}
-              {item.productCategory}
-              {item.price}
-            </p>
+        (list.length > 1) ?
 
-          </div>);
-        })
+          list.map((item) => {
+
+            return (<div className="container  d-flex flex-column bd-highlight mb-3" key={item.prodId}>
+              <div className="card  text-center p-2 bd-highlight" style={{ width: '18rem' }}>
+                <div className="card-body  text-center">
+                  <h5 className="card-title  text-center">{item.productName}</h5>
+                  <p className="card-text  text-center">{item.productCategory}</p>
+                  <p className="card-text  text-center">{item.price}</p>
+                  <p className="card-text  text-center">{item.prodId}</p>
+                </div>
+              </div>
+            </div>);
+          }
+
+          ) : <h1>Nothing To Show </h1>
 
       }
-    </div>
+    </div >
   );
 }
+
+
