4effd029af0d0580b95c4f19611e9a57bd5eaf6d
shubhamJawale <shubhamjawale11@gmail.com>
2021-08-29 02:13:18 +0530

    react strap is installed

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import { useEffect } from "react";
 // import { start } from "@popperjs/core";
 import { Cardb } from "./cadrfile";
 import { ToastContainer, toast } from 'react-toastify';
+import "./card.css";
+import Form from "./form";
+import { Card, CardBody, CardHeader, CardText } from "reactstrap";
+
 
 export default function App() {
   const [list, setlist] = useState([{}]);
@@ -17,19 +21,21 @@ export default function App() {
   let test = () => {
     axios.get("http://localhost:9096/gp").then(
       (response) => {
-        //console.log(response);
-        let data = response.data;
-        console.log(response.data[0]);
+        // console.log(response);
+        //let data = response.data;
+        // console.log(list);
+        //console.log(response.data[0]);
         // let list2 = [{ prodId: 1, productName: 'shubham', productCategory: 'shubham', price: 23.45 }];
         setlist(response.data);
         toast.success('Data Loaded');
 
         //setlist(...list, { prodId: 0, productName: 'shubham', productCategory: 'shubham', price: 23.45 });
+        console.log(list);
         //console.log(list);
       },
       (error) => {
         console.log(error);
-        console.log("be5ta error alla");
+        console.log("Not Connected");
         toast.error('Sorry Something Went Wrong');
       }
 
@@ -44,25 +50,46 @@ export default function App() {
   let [list1, setlist1] = useState([])
 
   let i1 = 0;
-  list1 = ['hello', 'shubham', 'jawale'];
+  list1 = [];
+
+  console.log(list);
   return (
-    <div className="container">
+    <div className="maincontainer">
+
       <ToastContainer />
       {
         (list.length > 1) ?
 
           list.map((item) => {
 
-            return (<div className="container  d-flex flex-column bd-highlight mb-3" key={item.prodId}>
-              <div className="card  text-center p-2 bd-highlight" style={{ width: '18rem' }}>
-                <div className="card-body  text-center">
-                  <h5 className="card-title  text-center">{item.productName}</h5>
-                  <p className="card-text  text-center">{item.productCategory}</p>
-                  <p className="card-text  text-center">{item.price}</p>
-                  <p className="card-text  text-center">{item.prodId}</p>
+
+
+            return (
+              <div>
+                <Card key={item.prodId}>
+                  <CardHeader>{item.productName}</CardHeader>
+                  <CardBody>
+                    <CardText>{item.productCategory}</CardText>
+                    <CardText>{item.price}</CardText>
+                    <CardText>{item.prodId}</CardText>
+                  </CardBody>
+                </Card>
+
+
+                <div key={item.prodId}>
+
+                  <div className="card  text-center p-2 bd-highlight" style={{ margin: '20px', width: '22rem' }}>
+                    <div className="card-body  text-center">
+                      <h5 className="card-title  text-center" style={{ overflow: 'hidden' }}>Product Name : {item.productName}</h5>
+                      <p className="card-text  text-center">Product Category : {item.productCategory}</p>
+                      <p className="card-text  text-center">Product Price : {item.price}</p>
+                      <p className="card-text  text-center">Product id :{item.prodId}</p>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>);
+
+            );
           }
 
           ) : <h1>Nothing To Show </h1>
